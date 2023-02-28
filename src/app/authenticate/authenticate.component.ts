1ebb044b14235380c5ddb12355a622789066d1f0
stefa <stefanp1999@yahoo.com>
2023-02-28 22:27:56 +0200

    bug in auth after login, added msgs for auth

--- src/app/authenticate/authenticate.component.ts
+++ src/app/authenticate/authenticate.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 
 @Component({
@@ -11,9 +11,13 @@ import { Router } from '@angular/router';
 export class AuthenticateComponent implements OnInit {
   registrationForm: FormGroup;
   loginForm: FormGroup;
+  resetPasswordForm: FormGroup;
+  invalidPassword: boolean;
+  successfulRegistration: boolean;
 
   private registrationUrl = 'http://localhost:8080/auth/register';
   private authenticateUrl = 'http://localhost:8080/auth/authenticate';
+  private resetPasswordUrl = 'http://localhost:8080/auth/email-reset-password';
   constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
@@ -28,22 +32,31 @@ export class AuthenticateComponent implements OnInit {
       username: new FormControl('', Validators.required),
       password: new FormControl('', Validators.required)
     });
+    this.resetPasswordForm = new FormGroup({
+      emailAddress: new FormControl('', [Validators.required, Validators.email])
+    })
   }
 
   onRegistrationSubmit() {
+    this.successfulRegistration = false;
     const formValue = this.registrationForm.value;
 
     this.http.post(this.registrationUrl, formValue).subscribe(
       (response) => {
         console.log('Registration successful!', response);
-        this.router.navigate(['/authenticate']);
+        this.registrationForm.reset();
+        this.successfulRegistration = true;
+        // this.loginForm.setValue({ username: this.registrationForm.value.username, password: this.registrationForm.value.password })
+        // this.onLoginSubmit();
       },
       (error) => {
         console.error('Registration failed!', error);
       }
     );
   }
+
   onLoginSubmit() {
+    this.invalidPassword = false;
     const formValue = this.loginForm.value;
 
     this.http.post(this.authenticateUrl, formValue).subscribe(
@@ -54,8 +67,23 @@ export class AuthenticateComponent implements OnInit {
       },
       (error) => {
         console.error('Authenticate failed!', error);
+        this.invalidPassword = true;
       }
     );
+  }
+
+  resetPassword() {
+    const params = new HttpParams().set('emailAddress', this.resetPasswordForm.value.emailAddress);
+
+    this.http.post(this.resetPasswordUrl, null, { params }).subscribe(
+      (response: any) => {
+        console.log('Sent Reset Password Email!', response);
+      },
+      (error) => {
+        console.error('Authenticate failed!', error);
+      }
+    );
+    this.resetPasswordForm.reset();
   }
 
 
